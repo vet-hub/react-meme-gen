d897f32c390347e91670e27b1a1762acdc163702
vet <vetefir@gmail.com>
2023-03-28 11:44:36 +0200

    random image button

--- src/App.js
+++ src/App.js
@@ -41,7 +41,8 @@ export default App;
 
 function MyMeme(){
 
-	const [ data, setData ] = useState( null );
+	const [data, setData] = useState( null );
+	const [idx, setIdx] = useState( 0 );
 	const [inputText0, setInputText0] = useState('my Text 1');
 	const [inputText1, setInputText1] = useState('my Text 2');
 
@@ -71,66 +72,73 @@ function MyMeme(){
 	data && console.log('memeData[0].name:', data.memes[0].name);
 	data && console.log('memeData[0].url:', data.memes[0].url);
 
-
+	function getRandomImage() {
+		const randomMemeIndex = Math.floor(Math.random() * data?.memes.length);
+		setIdx(randomMemeIndex);
+  	}
 	
 
-const objToParam = (obj) => {
-	const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`)
-	return '?' + params.join('&')
-}
+	const objToParam = (obj) => {
+		const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`)
+		return '?' + params.join('&')
+	}
 
 
 
 	return (
-		<>
-			
-			
-			<h1>Meme Gen</h1>
-			<h2>{ data?.memes[0].name }</h2>
+	<>
+		
+		
+		<h1>Meme Gen</h1>
+		<h2>{ data?.memes[idx].name }</h2>
+		<h3>{ data?.memes[idx].id }</h3>
+
+		<button onClick={getRandomImage}>
+			Change meme image
+		</button>
+
+		<form 
+			onSubmit={async e => {
+				e.preventDefault();
+				const params = {
+					template_id: data.memes[idx].id,
+					username: process.env.REACT_APP_IMGFLIP_USERNAME,
+					password: process.env.REACT_APP_IMGFLIP_PASSWORD,
+					text0: inputText0,
+					text1: inputText1
+				}
+				const response = await fetch(`https://api.imgflip.com/caption_image${objToParam(params)}`, { 
+					method: "POST"
+				 })
+				const mydata = await response.json();
+				console.log(mydata);
+
+			}}			
+		>
 
 
+			<input className='inputText0' type="text" name="inputText0" id="inputText0" 
+				value={inputText0}
+				onChange={ e => { setInputText0(e.target.value) } }
+			/>			
 
-			<form 
-				onSubmit={async e => {
-					e.preventDefault();
-					const params = {
-						template_id: data.memes[0].id,
-						username: '',
-						password: '',
-						text0: inputText0,
-						text1: inputText1
-					}
-					const response = await fetch(`https://api.imgflip.com/caption_image${objToParam(params)}`)
-					const mydata = await response.json();
-					console.log(mydata);
+			<input className='inputText1' type="text" name="inputText1" id="inputText1" 
+				value={inputText1}
+				onChange={ e => { setInputText1(e.target.value) } }
+			/>
 
-				}}			
-			>
+			<button type='submit'>new meme</button>
 
-				<input className='inputText0' type="text" name="inputText0" id="inputText0" 
-					value={inputText0}
-					onChange={ e => { setInputText0(e.target.value) } }
-				/>			
-
-				<input className='inputText1' type="text" name="inputText1" id="inputText1" 
-					value={inputText1}
-					onChange={ e => { setInputText1(e.target.value) } }
-				/>
-
-				<button type='submit'>new meme</button>
-
-			</form>
+		</form>
 
 
-			<div className='memeContainer'>
-				<div className='memeText0'>{inputText0}</div>
-				<img src={data?.memes[0].url} alt={data?.memes[0].name} />
-				<div className='memeText1'>{inputText1}</div>
-			</div>
+		<div className='memeContainer'>
+			<div className='texts memeText0'>{inputText0}</div>
+			<img src={data?.memes[idx].url} alt={data?.memes[idx].name} />
+			<div className='texts memeText1'>{inputText1}</div>
+		</div>
 
-
-
-		</>
+	</>
 	)
 }
 
